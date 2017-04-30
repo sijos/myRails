e06a309620d7416ac46d7e8669200b71dd4f15b7
Scott Snyder <ssny88@gmail.com>
2017-04-30 15:58:21 -0700

    setup render method for base controller

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -39,6 +40,10 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.name.underscore
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    template = ERB.new(File.read(path))
+    render_content(template.result(binding), "text/html")
   end
 
   # method exposing a `Session` object
