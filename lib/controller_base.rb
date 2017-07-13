4cd2ec3e53ec67453b213c93f787f84b73bda309
Scott Snyder <ssny88@gmail.com>
2017-07-12 18:21:56 -0700

    clean up comments in controllerBase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -15,7 +15,6 @@ class ControllerBase
     @params = params
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
     @already_built_response
   end
@@ -31,7 +30,6 @@ class ControllerBase
 
   # Populate the response with content.
   # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise 'Double render error' if already_built_response?
     @res['Content-Type'] = content_type
